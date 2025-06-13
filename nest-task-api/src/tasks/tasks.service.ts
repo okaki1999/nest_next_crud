3d7f2a883ec42087b0ad2cb1c41edfb3312e1fd7
Fujita <okakilikecat@gmail.com>
2025-06-14 05:20:10 +0900

    初めてのコミット

---
+++ nest-task-api/src/tasks/tasks.service.ts
@@ -0,0 +1,45 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
+import { Task } from './entities/task.entity';
+
+@Injectable()
+export class TasksService {
+  private tasks: Task[] = [];
+  private idCounter = 1;
+
+  create(createTaskDto: CreateTaskDto): Task {
+    const task: Task = {
+      id: this.idCounter++,
+      ...createTaskDto,
+    };
+    this.tasks.push(task);
+    return task;
+  }
+
+  findAll(): Task[] {
+    return this.tasks;
+  }
+
+  findOne(id: number): Task {
+    const task = this.tasks.find(t => t.id === id);
+    if (!task) {
+      throw new NotFoundException('Task not found');
+    }
+    return task;
+  }
+
+  update(id: number, updateTaskDto: UpdateTaskDto): Task {
+    const task = this.findOne(id);
+    Object.assign(task, updateTaskDto);
+    return task;
+  }
+
+  remove(id: number): void {
+    const index = this.tasks.findIndex(t => t.id === id);
+    if (index === -1) {
+      throw new NotFoundException('Task not found');
+    }
+    this.tasks.splice(index, 1);
+  }
+}
